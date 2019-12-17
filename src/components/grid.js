2ce4b66d135c0a1dc9881bcd6d52950f9bf83704
Dan Woo <daniel4wood@icloud.com>
2019-12-17 11:42:32 +0100

    Remove unnecessary files and code

--- src/components/grid.js
+++ src/components/grid.js
@@ -1,28 +1,10 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
 import MediaCard from "./media-card"
 
-const Grid = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      allAlbumsJson {
-        nodes {
-          title
-          artist
-          src {
-            childImageSharp {
-              fluid(maxWidth: 2000) {
-                ...GatsbyImageSharpFluid
-              }
-            }
-          }
-        }
-      }
-    }
-  `)
+const Grid = ({ json }) => {
   return (
     <div className="grid grid-columns-2 sm:grid-columns-3 md:grid-columns-4 lg:grid-columns-5 xl:grid-columns-6 grid-gap-4 my-4">
-      {data.allAlbumsJson.nodes.map(album => (
+      {json.nodes.map(album => (
         <MediaCard
           image={album.src.childImageSharp.fluid}
           title={album.title}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,34 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Grid from "../components/grid"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Grid />
-  </Layout>
-)
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allAlbumsJson {
+        nodes {
+          title
+          artist
+          src {
+            childImageSharp {
+              fluid(maxWidth: 2000) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Grid json={data.allAlbumsJson} />
+    </Layout>
+  )
+}
 
 export default IndexPage
